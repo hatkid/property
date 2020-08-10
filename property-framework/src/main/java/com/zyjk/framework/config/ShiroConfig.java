c94540dce860e68b24f5d4b39ede461ced5bb342
lvzhi <lvzhi1988@126.com>
2020-08-10 21:51:35 +0800

    模板简单修改

--- property-framework/src/main/java/com/zyjk/framework/config/ShiroConfig.java
+++ property-framework/src/main/java/com/zyjk/framework/config/ShiroConfig.java
@@ -279,6 +279,9 @@ public class ShiroConfig
         filterChainDefinitionMap.put("/ajax/**", "anon");
         filterChainDefinitionMap.put("/js/**", "anon");
         filterChainDefinitionMap.put("/lz/**", "anon");
+        filterChainDefinitionMap.put("/module/**", "anon");
+        filterChainDefinitionMap.put("/assets/**", "anon");
+        filterChainDefinitionMap.put("/data/**", "anon");
         filterChainDefinitionMap.put("/captcha/captchaImage**", "anon");
         // 退出 logout地址，shiro去清除session
         filterChainDefinitionMap.put("/logout", "logout");
